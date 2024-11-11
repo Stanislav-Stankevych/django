c2aae067e3d78ca2f77b91fcf92c0aae9569995e
Stanyslav <nitro04719@gmail.com>
2024-11-11 15:24:19 +0100

    Переустановк tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,10 @@
 // postcss.config.js
 module.exports = {
   plugins: [
+    require("postcss-import"), // Подключите postcss-import первым
     require("tailwindcss"),
-    require("postcss-nested"),
     require("autoprefixer"),
+    require("postcss-nested"),
     require("cssnano")({
       preset: "default",
     }),
